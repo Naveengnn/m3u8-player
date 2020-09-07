2dd95c37322545a56b00b4f814844633324bb616
Suyash Jain <53927129+Suyashjain05@users.noreply.github.com>
2020-09-07 11:48:09 +0530

    removed base64 encryption

--- main.js
+++ main.js
@@ -1,8 +1,7 @@
-var link = btoa('#m3u8-placeholder');
 $(window).on('load', function () {
     $('#m3u8-placeholder')[0].value = localStorage.getItem('m3u8-link') || '';
     $('#play-btn').on('click', function () {
-        localStorage.setItem('m3u8-link', $(link)[0].value);
-        window.location.href = './player' + '#' + $(link)[0].value;
+        localStorage.setItem('m3u8-link', $('#m3u8-placeholder')[0].value);
+        window.location.href = './player' + '#' + $('#m3u8-placeholder')[0].value;
     });
 });
